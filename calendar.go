6bb85646ef3121e98e057f4e1a1b11d7287bb4bb
Lewis_Damy <ldamy16@gmail.com>
2025-05-30 12:15:21 -0300

    feat: added basic calendar interface with struct

---
+++ calendar.go
@@ -0,0 +1,42 @@
+package go_calendar
+
+import "github.com/lewisdamy/go-calendar/pkg/date"
+
+type HolidayCalendar struct {
+	weekdayHolidays  []string
+	specificHolidays []date.Date
+}
+
+func (c *HolidayCalendar) SetWeekdayHoliday(DayOfTheWeek string) {
+	c.weekdayHolidays = append(c.weekdayHolidays, DayOfTheWeek)
+}
+
+func (c *HolidayCalendar) SetDayHoliday(year, month, day int) {
+	c.specificHolidays = append(c.specificHolidays, date.NewDate(year, month, day))
+}
+
+func (c *HolidayCalendar) SetDayOfMonthHoliday(month, day int) {
+
+}
+
+func (c *HolidayCalendar) isWeekdayHoliday(inputDate date.Date) bool {
+	for _, v := range c.weekdayHolidays {
+		if inputDate.Weekday() == v {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *HolidayCalendar) isSpecificHoliday(inputDate date.Date) bool {
+	for _, v := range c.specificHolidays {
+		if v == inputDate {
+			return true
+		}
+	}
+	return false
+}
+
+func (c HolidayCalendar) IsHoliday(date date.Date) bool {
+	return c.isWeekdayHoliday(date) || c.isSpecificHoliday(date)
+}
